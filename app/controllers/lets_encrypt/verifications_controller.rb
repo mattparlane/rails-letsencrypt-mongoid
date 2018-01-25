671fd57fa702a05c559dd6cd0d71b49acc24d504
Nathan Broadbent <git@ndbroadbent.com>
2018-01-25 14:48:54 +0700

    Support Rails 4.0 with render text: ...

--- app/controllers/lets_encrypt/verifications_controller.rb
+++ app/controllers/lets_encrypt/verifications_controller.rb
@@ -3,17 +3,21 @@
 require_dependency 'lets_encrypt/application_controller'
 
 module LetsEncrypt
+  TEXT_OR_PLAIN = Rails::VERSION::MAJOR > 4 || (
+    Rails::VERSION::MAJOR == 4 && Rails::VERSION::MINOR >= 1
+  ) ? :plain : :text
+
   # :nodoc:
   class VerificationsController < ApplicationController
     def show
       return render_verification_string if certificate.present?
-      render plain: 'Verification not found', status: 404
+      render TEXT_OR_PLAIN => 'Verification not found', status: 404
     end
 
     protected
 
     def render_verification_string
-      render plain: certificate.verification_string
+      render TEXT_OR_PLAIN => certificate.verification_string
     end
 
     def certificate
